a5095c3934968e09dcf7965f9629e7f55a0f4501
davilbs <davi.ativa@gmail.com>
2022-11-26 21:57:45 -0300

    Commit 14

--- Price.java
+++ Price.java
@@ -1,5 +1,11 @@
 public abstract class Price {
   public abstract int getPriceCode();
+  public int getFrequentRenterPoints(int daysRented) {
+    // add bonus for a two day new release rental
+    if ((this.getPriceCode() == Movie.NEW_RELEASE) &&
+            daysRented > 1) return 2;
+    return 1;
+  }
   public double getCharge(int daysRented) {
     double thisAmount = 0;
     switch (this.getPriceCode()) {
