c4bbf7421d1694fa000118bfc7f43fb69c3b49d6
davilbs <davi.ativa@gmail.com>
2022-11-26 22:00:28 -0300

    Commit 13

--- Price.java
+++ Price.java
@@ -6,23 +6,5 @@ public abstract class Price {
             daysRented > 1) return 2;
     return 1;
   }
-  public double getCharge(int daysRented) {
-    double thisAmount = 0;
-    switch (this.getPriceCode()) {
-       case Movie.REGULAR:
-          thisAmount += 2;
-          if (daysRented > 2)
-             thisAmount += (daysRented - 2) * 1.5;
-          break;
-       case Movie.NEW_RELEASE:
-          thisAmount += daysRented * 3;
-          break;
-       case Movie.CHILDRENS:
-          thisAmount += 1.5;
-          if (daysRented > 3)
-             thisAmount += (daysRented - 3) * 1.5;
-          break;
-    }
-    return thisAmount;
-  }
+  public abstract double getCharge(int daysRented);
 }
